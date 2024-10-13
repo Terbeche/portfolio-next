509c93fa866503249d71a4b485f22b076a7a2472
Mostefa-Terbeche <termus96@gmail.com>
2024-10-13 18:35:05 +0100

    Fix project layout

--- src/components/projects/ProjectLayout.tsx
+++ src/components/projects/ProjectLayout.tsx
@@ -27,24 +27,23 @@ const ProjectLayout: React.FC<ProjectLayoutProps> = ({ name, description, demoLi
       className="text-sm md:text-base flex flex-col md:flex-row items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg"
     >
       <div className="flex items-center justify-center space-x-2">
-        <Image src={featuredImage} alt={name} width={64} height={64} className="w-12 h-12 sm:w-16 sm:h-16" />
         <div className="flex flex-col">
-          <h2 className="text-foreground">{name}</h2>
-          <p className="text-muted hidden sm:inline-block">{description}</p>
+          <h2 className="text-foreground mb-4">{name}</h2>
+          <p className="text-gray-400 hidden sm:inline-block mb-4">{description}</p>
           <ul className="flex space-x-2">
             {technologies.map((tech) => (
               <li key={tech} className="text-accent">{tech}</li>
             ))}
           </ul>
+          <div className="flex items-end space-y-1 mt-4 md:mt-0 gap-x-2 text-lg space-x-12 text-sky-400">
+            <Link href={demoLink} target="_blank" className="text-primary">
+              Demo Link
+            </Link>
+            <Link href={sourceLink} target="_blank" className="text-primary">
+              Source Link
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-end space-y-1 mt-4 md:mt-0">
-        <Link href={demoLink} target="_blank" className="text-primary underline">
-          Demo Link
-        </Link>
-        <Link href={sourceLink} target="_blank" className="text-primary underline">
-          Source Link
-        </Link>
       </div>
     </ProjectLink>
   );
